d50d0144c017c6b40a02029305ba3b47f299d129
Pahrul Roji <fahrul3@gmail.com>
2023-02-25 01:13:38 +0700

    Web Moza
    Moza Cell

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Toggle class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamberger menu di klik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//Klik di luar side bar untuk menghilangkan nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
